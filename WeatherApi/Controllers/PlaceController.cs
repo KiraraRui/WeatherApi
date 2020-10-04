33051fba5bf321e22b4505c1b75a88ddab759520
ShizukuKiri <kira.kira@live.dk>
2020-10-05 00:08:45 +0200

    First commit

---
+++ WeatherApi/Controllers/PlaceController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WeatherApi.Data;
+using WeatherApi.Models;
+
+namespace WeatherApi.Controllers
+{
+    public class PlaceController : ApiController
+    {
+        private DataContext dataContext;
+        public PlaceController()
+        {
+            dataContext = new DataContext();
+        }
+
+        // GET api/values
+        [Route("api/getall")]
+        public List<Place> GetAll()
+        {
+            return dataContext.GetAllPlaces();
+        }
+
+        // GET api/values/5
+        [Route("api/get")]
+        public Place Get(int id)
+        {
+            return dataContext.GetPlace(id);
+        }
+
+       
+    }
+}
---
+++ WeatherApi/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using WeatherApi.Models;
+
+namespace WeatherApi.Data
+{
+    public class DataContext
+    {
+        private string connectionString = "Server=193.106.164.115;Port=3306;Database=mydb;Uid=michael;Pwd=IDKm4n0r4ng3S33msKind4SUS!;";
+
+        public Place GetPlace(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("idIn", id);
+
+                return connection.QueryFirstOrDefault<Place>("GetPlaceWithId", parameters, commandType: CommandType.StoredProcedure, commandTimeout: 10);
+            }
+        }
+
+
+        public List<Place> GetAllPlaces()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+             return connection.Query<Place>("GetAllPlaces", null, commandType: CommandType.StoredProcedure, commandTimeout: 10).ToList();
+            }
+        }
+
+    }
+}
